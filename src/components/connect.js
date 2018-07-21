afdcba95031dd8c869c5d153f04af6ff665343b2
Courtney Adams <cadams8238@gmail.com>
2018-07-20 21:57:33 -0700

    added links to connect component, fixed space around icons by replacing them with new trimmed svgs

--- src/components/connect.js
+++ src/components/connect.js
@@ -9,21 +9,37 @@ import linkedin from '../images/contact_linkedin.svg';
 export default function LandingPage() {
     return (
         <div className={styles.backgroundImage}>
-            <h2 className={[styles.heading, styles.firstHeading].join(' ')}>Let's</h2>
-            <h2 className={styles.heading}>Connect</h2>
+            <div className={styles.allHeaderText}>
+                <h2 className={[styles.heading, styles.firstHeading].join(' ')}>Let's</h2>
+                <h2 className={styles.heading}>Connect</h2>
+            </div>
 
             <div className={styles.icons}>
-                <img src={github}
-                    alt="Github icon" />
-                <img src={email}
-                    alt="Email icon" />
-                <img src={linkedin}
-                    alt="LinkedIn icon" />
+                <a href="https://github.com/cadams8238">
+                    <img className={styles.iconImg}
+                        src={github}
+                        alt="Github icon"
+                    />
+                </a>
+
+                <a href="mailto:cadams8238@gmail.com">
+                    <img className={styles.iconImg}
+                        src={email}
+                        alt="Email icon"
+                    />
+                </a>
+
+                <a href="https://www.linkedin.com/in/coadams88/">
+                    <img className={[styles.iconImg, styles.linkedin].join(' ')}
+                        src={linkedin}
+                        alt="LinkedIn icon"
+                    />
+                </a>
             </div>
 
             <div className={styles.supportingText}>
-            <p>Looking forward to hearing</p>
-            <p>from you!</p>
+                <p>Looking forward to hearing</p>
+                <p>from you!</p>
             </div>
         </div>
     );
