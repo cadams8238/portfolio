e7250091f743f9549908ff429c618afe2f3f0f9e
Courtney Adams <cadams8238@gmail.com>
2018-07-27 22:06:34 -0700

    added circle accents for larger media queries on landing page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -7,18 +7,23 @@ export default function LandingPage() {
 
     return (
         <div className={styles.background}>
-            
+
             <header>
-                <p className={styles.hello}>Hello!</p>
-                <p className={styles.myNameIs}>My name is</p>
+              <div className={styles.circle1}></div>
+              <div className={styles.circle2}></div>
+              <div className={styles.circle3}></div>
+              <div className={styles.circle4}></div>
 
-                <div className={styles.nameAndDesc}>
-                    <h1 className={styles.nameHeading}>Courtney</h1>
-                    <h1 className={styles.nameHeading}>Adams</h1>
+              <p className={styles.hello}>Hello!</p>
+              <p className={styles.myNameIs}>My name is</p>
 
-                    <p className={[styles.description, styles.firstDesc].join(' ')}>I'm a fullstack developer and designer based</p>
-                    <p className={styles.description}>in the beautiful PNW</p>
-                </div>
+              <div className={styles.nameAndDesc}>
+                  <h1 className={styles.nameHeading}>Courtney</h1>
+                  <h1 className={styles.nameHeading}>Adams</h1>
+
+                  <p className={[styles.description, styles.firstDesc].join(' ')}>I'm a fullstack developer and designer based</p>
+                  <p className={styles.description}>in the beautiful PNW</p>
+              </div>
 
             </header>
         </div>
