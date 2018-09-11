c969a35e6bac68f29752ee2c9bc86e2036e99f2e
Courtney Adams <cadams8238@gmail.com>
2018-09-11 10:09:36 -0700

    updated positioning of landing page on smallest mobile view to use flex instead of absolute positioning

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -7,16 +7,10 @@ export default function LandingPage() {
 
     return (
         <div className={styles.background}>
-
-            <header>
-              <div className={styles.circle1}></div>
-              <div className={styles.circle2}></div>
-              <div className={styles.circle3}></div>
-              <div className={styles.circle4}></div>
-
+          <header className={styles.flexContainer}>
+            <div>
               <p className={styles.hello}>Hello!</p>
               <p className={styles.myNameIs}>My name is</p>
-
               <div className={styles.nameAndDesc}>
                   <h1 className={styles.nameHeading}>Courtney</h1>
                   <h1 className={styles.nameHeading}>Adams</h1>
@@ -24,8 +18,12 @@ export default function LandingPage() {
                   <p className={[styles.description, styles.firstDesc].join(' ')}>I'm a fullstack developer and designer based</p>
                   <p className={styles.description}>in the beautiful PNW</p>
               </div>
-
-            </header>
+            </div>
+          </header>
+          <div className={styles.circle1}></div>
+          <div className={styles.circle2}></div>
+          <div className={styles.circle3}></div>
+          <div className={styles.circle4}></div>
         </div>
     );
 }
