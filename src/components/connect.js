003f95eb489b275daa098631085e5b26be2eead6
Courtney Adams <cadams8238@gmail.com>
2018-09-11 12:34:34 -0700

    updated positioning to use flexbox on connect component

--- src/components/connect.js
+++ src/components/connect.js
@@ -11,43 +11,47 @@ export default function Connect() {
         <div className={styles.backgroundImage}
           id="connect"
         >
+          <section className={styles.flexContainer}>
+            <div>
+              <div className={styles.allHeaderText}>
+                  <h2 className={[styles.heading, styles.firstHeading].join(' ')}>Let's</h2>
+                  <h2 className={styles.heading}>Connect</h2>
+              </div>
+
+              <div className={styles.icons}>
+                  <a href="https://github.com/cadams8238">
+                      <img className={styles.iconImg}
+                          src={github}
+                          alt="Github icon"
+                      />
+                  </a>
+
+                  <a href="mailto:cadams8238@gmail.com">
+                      <img className={styles.iconImg}
+                          src={email}
+                          alt="Email icon"
+                      />
+                  </a>
+
+                  <a href="https://www.linkedin.com/in/coadams88/">
+                      <img className={[styles.iconImg, styles.linkedin].join(' ')}
+                          src={linkedin}
+                          alt="LinkedIn icon"
+                      />
+                  </a>
+              </div>
+
+              <div className={styles.supportingText}>
+                  <p>Looking forward to hearing</p>
+                  <p>from you!</p>
+              </div>
+            </div>
+          </section>
+
           <div className={styles.circle1}></div>
           <div className={styles.circle2}></div>
           <div className={styles.circle3}></div>
           <div className={styles.circle4}></div>
-
-            <div className={styles.allHeaderText}>
-                <h2 className={[styles.heading, styles.firstHeading].join(' ')}>Let's</h2>
-                <h2 className={styles.heading}>Connect</h2>
-            </div>
-
-            <div className={styles.icons}>
-                <a href="https://github.com/cadams8238">
-                    <img className={styles.iconImg}
-                        src={github}
-                        alt="Github icon"
-                    />
-                </a>
-
-                <a href="mailto:cadams8238@gmail.com">
-                    <img className={styles.iconImg}
-                        src={email}
-                        alt="Email icon"
-                    />
-                </a>
-
-                <a href="https://www.linkedin.com/in/coadams88/">
-                    <img className={[styles.iconImg, styles.linkedin].join(' ')}
-                        src={linkedin}
-                        alt="LinkedIn icon"
-                    />
-                </a>
-            </div>
-
-            <div className={styles.supportingText}>
-                <p>Looking forward to hearing</p>
-                <p>from you!</p>
-            </div>
         </div>
     );
 }
