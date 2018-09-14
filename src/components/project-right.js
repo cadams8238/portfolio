09c7fceef12daa8062f04bd45bfc508c1a47b733
Courtney Adams <cadams8238@gmail.com>
2018-09-13 17:22:20 -0700

    updated right component to match responsiveness of left component

--- src/components/project-right.js
+++ src/components/project-right.js
@@ -8,29 +8,31 @@ export default function ProjectRight(props) {
   return (
     <div className={styles.backgroundImage}>
       <div className={styles.projectBackground}></div>
-      <img className={styles.projectPhoto}
-        src={props.content.img.src}
-        alt={props.content.img.alt}
-      />
-      <section className={styles.allText}>
-        <h2 className={styles.title}>
-          {props.content.title}
-        </h2>
-        <p className={styles.description}>
-          {props.content.description}
-        </p>
-        <a href={props.content.links.app}>
-          <Button label="Live App"
-            className={styles.buttonLinks}
-          />
-        </a>
-        <a href={props.content.links.code}>
-          <Button label="See Code"
-            className={styles.button}
-          />
-        </a>
-        <p className={styles.roles}>Tech Stack: {props.content.stack}</p>
-      </section>
+      <div className={styles.flexContainer}>
+        <img className={styles.projectPhoto}
+          src={props.content.img.src}
+          alt={props.content.img.alt}
+        />
+        <section className={styles.allText}>
+          <h2 className={styles.title}>
+            {props.content.title}
+          </h2>
+          <p className={styles.description}>
+            {props.content.description}
+          </p>
+          <a href={props.content.links.app}>
+            <Button label="Live App"
+              className={styles.buttonLinks}
+            />
+          </a>
+          <a href={props.content.links.code}>
+            <Button label="See Code"
+              className={styles.button}
+            />
+          </a>
+          <p className={styles.roles}>Tech Stack: {props.content.stack}</p>
+        </section>
+      </div>
     </div>
   );
 }
