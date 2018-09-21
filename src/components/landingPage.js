e117f4df79ce10db23dd3a92be6bd24bfc28bb05
Courtney Adams <cadams8238@gmail.com>
2018-09-21 11:45:54 -0700

    updated description on landing page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -15,8 +15,11 @@ export default function LandingPage() {
                   <h1 className={styles.nameHeading}>Courtney</h1>
                   <h1 className={styles.nameHeading}>Adams</h1>
 
-                  <p className={[styles.description, styles.firstDesc].join(' ')}>I'm a fullstack developer and designer based</p>
-                  <p className={styles.description}>in the beautiful PNW</p>
+                  <p className={[styles.description, styles.firstDesc].join(' ')}>I'm a fullstack developer and designer based in the</p>
+                  <p className={styles.description}>beautiful PNW. I'm an avid problem solver looking to</p>
+                  <p className={styles.description}>join a rad, collaborative team where my skills and</p>
+                  <p className={styles.description}>passions can shine!</p>
+
               </div>
             </div>
           </header>
